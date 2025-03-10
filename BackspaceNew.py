727d4dabf22c9997841bc9325acb66a926528053
jibraniqbal <43417430+jibraniqbal90@users.noreply.github.com>
2025-03-10 22:34:12 +0400

    Update BackspaceNew.py

--- BackspaceNew.py
+++ BackspaceNew.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-# Created by linuxitos
-# Updated to prevent backspace from going back when renaming a file/folder in Nautilus
+
+# created by linuxitos
+# Changed from win.up to slot.up to work with gnome47 and fedora41
 
 import gi
 
@@ -8,21 +9,32 @@ gi.require_version('Nautilus', '4.0')
 gi.require_version('Gtk', '4.0')
 from gi.repository import GObject, Nautilus, Gtk, GLib
 
+
 def idle_callback(*args):
+    # Get the default application
     app = Gtk.Application.get_default()
     
-    # Check if the focus is not on a text entry (for renaming) to allow backspace to go back
-    if isinstance(app.get_active_window(), Gtk.Entry):
-        return False  # Don't do anything if we are in a text entry (renaming file/folder)
-    
-    # Otherwise, bind Backspace to the "Go Back" action
-    app.set_accels_for_action("slot.up", ["BackSpace"])
+    # Set the accelerator for Backspace only if we are not renaming a file/folder
+    if not isinstance(app.get_active_window(), Gtk.Entry):
+        app.set_accels_for_action("slot.up", ["BackSpace"])
     return False
 
+
 def window_added(*args):
+    # This ensures that the callback is called when a new window is added
     GLib.idle_add(idle_callback, None)
 
+
 class BackspaceBack(GObject.GObject, Nautilus.ColumnProvider):
-    app = Gtk.Application.get_default()
-    app.set_accels_for_action("slot.up", ["BackSpace"])
-    app.connect("window-added", window_added)
+    def __init__(self):
+        super().__init__()
+        # Get the default application
+        self.app = Gtk.Application.get_default()
+        # Initially, bind Backspace for "Go Back" action
+        self.app.set_accels_for_action("slot.up", ["BackSpace"])
+        # Connect to window-added signal
+        self.app.connect("window-added", window_added)
+
+
+# Instantiate and activate the script
+BackspaceBack()
